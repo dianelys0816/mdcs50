fec3e90fad0a59308927a4a4ed6aa6af9a1c4d2f
dianelys0816 <dianelys0816@gmail.com>
2015-06-11 22:46:48 -0400

    first part of sectioning website

--- web/bootstrap.php
+++ web/bootstrap.php
@@ -6,3 +6,47 @@ $website_settings =[
     'copyright' => 'Copyright &copy; DJ %s',
     'home' => 'DJ'
 ];
+
+$portfolio = [
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+    [
+        'container_class' => 'col-md-3 img-portfolio',
+        'img_class' => 'img-responsive img-hover',
+        'portfolio_link' => 'portfolio-item.php',
+        'img_link' => 'http://lorempixel.com/g/750/450',
+    ],
+];
+
+$home_carousel = [];
+
+$about_carousel = [
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide One',
+        'caption' => 'Caption 1',
+    ],
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide Two',
+        'caption' => 'Caption 2',
+    ],
+    [
+        'img_url' => 'http://placehold.it/1900x1080&text=Slide Three',
+        'caption' => 'Caption 3',
+    ]
+];
